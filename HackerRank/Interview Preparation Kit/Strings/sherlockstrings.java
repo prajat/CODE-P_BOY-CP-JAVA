b8450c833f046094a30e998e23db7b7d2b6f7929
prajat <rajat.singh1084@gmail.com>
2018-07-29 15:45:51 +0530

    update strings

--- HackerRank/Interview Preparation Kit/Strings/sherlockstrings.java
+++ HackerRank/Interview Preparation Kit/Strings/sherlockstrings.java
@@ -1,42 +1,62 @@
-import java.util.HashMap;
+import java.io.*;
 import java.util.*;
 
-/**
- * sherlockstrings
- */
-public class sherlockstrings {
-
-    public static HashMap<Character, Integer> map = new HashMap<>();
+public class Solution {
 
     public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
 
-        System.out.println(isvalid("aabbcd"));
+        Map<Character, Integer> frequencies = new HashMap<>();
 
-    }
-    
-
-    public static boolean isvalid(String str) {
-
-        for(int i=0;i<str.length();i++)
-        {
-            if (map.containsKey(str.charAt(i))) {
-                int c = map.get(str.charAt(i));
-                c++;
-                map.put(str.charAt(i), c);
-
-            } else {
-                map.put(str.charAt(i), 1);
-            }
+        for (char letter : s.toCharArray()) {
+            if (frequencies.containsKey(letter))
+                frequencies.put(letter, frequencies.get(letter) + 1);
+            else
+                frequencies.put(letter, 1);
         }
-        
-        List<Value> values = new ArrayList<Value>(map.values());
-        System.out.println(values);
-        
-        
-        
-        return true;
 
+        Set<Integer> st = new HashSet<>();
+        for (int freq : frequencies.values()) {
+            st.add(freq);
+        }
 
-        
+        if (st.size() > 2)// More than 2 frequencies
+            System.out.println("NO");
+        else if (st.size() == 1)
+            System.out.println("YES");
+        else// 2 different frequencies
+        {
+            int f1 = 0;
+            int f2 = 0;
+            int f1Count = 0;
+            int f2Count = 0;
+            int i = 0;
+            for (int n : st) {
+                if (i == 0)
+                    f1 = n;
+                else
+                    f2 = n;
+                i++;
+            }
+
+            for (int freq : frequencies.values()) {
+                if (freq == f1)
+                    f1Count++;
+                if (freq == f2)
+                    f2Count++;
+            }
+
+            if ((f1 == 1 && f1Count == 1) || (f2 == 1 && f2Count == 1))
+                System.out.println("YES");
+            else if ((Math.abs(f1 - f2) == 1) && (f1Count == 1 || f2Count == 1))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
     }
 }
