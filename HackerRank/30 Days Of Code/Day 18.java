f8b6733187a188aa01acd4de4392e7ab2a8012b5
prajat <rajat.singh1084@gmail.com>
2018-09-17 23:38:42 +0530

    day 18 completed

---
+++ HackerRank/30 Days Of Code/Day 18.java
@@ -0,0 +1,22 @@
+public class Solution {
+    // Write your code here.
+    Stack<Character> stack = new Stack();
+    Queue<Character> queue = new LinkedList();
+
+    public void pushCharacter(char c) {
+        stack.push(c);
+
+    }
+
+    public void enqueueCharacter(char c) {
+        queue.add(c);
+    }
+
+    public char popCharacter() {
+        return stack.pop();
+    }
+
+    public char dequeueCharacter(){
+        return queue.remove();
+        
+    }
