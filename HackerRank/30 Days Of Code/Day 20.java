2b8beda80fa1310f14d515ad8bfc3f55f4f223b5
prajat <rajat.singh1084@gmail.com>
2018-09-18 22:21:23 +0530

    Day 20 completed

---
+++ HackerRank/30 Days Of Code/Day 20.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
+        }
+        int totalSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            int numSwaps = 0;
+            for (int j = 0; j < n - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    numSwaps++;
+                }
+            }
+            if (numSwaps == 0) {
+                break;
+            }
+            totalSwaps += numSwaps;
+        }
+        // Write Your Code Here
+        System.out.println("Array is sorted in " + totalSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n - 1]);
+    }
+}
