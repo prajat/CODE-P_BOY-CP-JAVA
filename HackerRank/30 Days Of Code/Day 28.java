d700701e5376d3d1a1d06f2d0f0006393aaaf3e5
prajat <rajat.singh1084@gmail.com>
2018-10-02 12:38:32 +0530

    day 28 completed

---
+++ HackerRank/30 Days Of Code/Day 28.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        ArrayList<String> ans = new ArrayList<>();
+        for (int NItr = 0; NItr < N; NItr++) {
+            String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+            String firstName = firstNameEmailID[0];
+
+            String emailID = firstNameEmailID[1];
+
+            if (emailID.endsWith("@gmail.com")) {
+                ans.add(firstName);
+            }
+        }
+        Collections.sort(ans);
+        for (String i : ans) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+    }
+}
