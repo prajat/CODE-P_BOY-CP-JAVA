0412c6e6bee1237094bc08c2080db31c1c1be81b
prajat <rajat.singh1084@gmail.com>
2019-01-05 00:14:40 +0530

    ques solved

--- Coding ninjas contest/kaunbanegacrorepatti4jan2019/passingthenumber.java
+++ Coding ninjas contest/kaunbanegacrorepatti4jan2019/passingthenumber.java
@@ -2,7 +2,36 @@ import java.util.*;
 
 public class passingthenumber {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        for (int j = 1; j <= t; j++) {
+            int l = s.nextInt();
+            int h = s.nextInt();
+            int happiness = s.nextInt();
+            int count = 0;
+            int pr = 1;
+            
+            for (int i = l; i <= h; i++) {
+            while (i > 0) {
+                    int r = n % 10;
+                    pr = pr * r;
+                    i = i / 10;
 
+            }
+        }
+            
+
+        }
+
+    }
+
+    public static int happy(int l, int h) {
+        
+        for (int i = l; i <= h; i++) {
+            while (i > 0) {
+                int 
+            }
+        }
+        
     }
 }
