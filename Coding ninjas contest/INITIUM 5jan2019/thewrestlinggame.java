8c38d2922630437d750894527e21e23bc613b2c6
prajat <rajat.singh1084@gmail.com>
2019-01-05 22:50:53 +0530

    ques solved

--- Coding ninjas contest/INITIUM 5jan2019/thewrestlinggame.java
+++ Coding ninjas contest/INITIUM 5jan2019/thewrestlinggame.java
@@ -10,17 +10,24 @@ public class thewrestlinggame {
         int n = s.nextInt();
         int p = s.nextInt();
         int[] arr = new int[n];
-        Arrays.sort(arr);
-        int ans=n;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (arr[j] < arr[i] && arr[i] <= arr[j] + p) {
-                    ans--;
-                }
-                i = j - 1;
-            }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = s.nextInt();
         }
+        Arrays.sort(arr);
+        int count = 0;
+        for (int i = 1; i < arr.length; i++) {
 
-        System.out.println(ans);
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] > arr[j] && arr[i] < arr[j] + p) {
+                    count++;
+                }
+            }
+
+
+
+        }
+            
+
+        System.out.println(n-count);
     }
 }
