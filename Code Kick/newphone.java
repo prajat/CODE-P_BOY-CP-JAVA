f50e3e48a69e7fc0b80b8ccb63eb398c724b721a
prajat <rajat.singh1084@gmail.com>
2019-01-16 23:15:56 +0530

    ques solved

---
+++ Code Kick/newphone.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        
+
+
+    }
+
+}
