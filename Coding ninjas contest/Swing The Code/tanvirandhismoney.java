b47d749a2c52ed175fa4b4a81c450c9eda5eac1a
prajat <rajat.singh1084@gmail.com>
2019-01-26 21:52:25 +0530

    ques solved

--- Coding ninjas contest/Swing The Code/tanvirandhismoney.java
+++ Coding ninjas contest/Swing The Code/tanvirandhismoney.java
@@ -4,13 +4,13 @@ public class tanvirandhismoney {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // int t = in.nextInt();
-        // for (int i = 1; i <= t; i++) {
-        //     int n = in.nextInt();
-        //     int ans = count(n);
-        //     System.out.println(ans % 2);
+        int t = in.nextInt();
+        for (int i = 1; i <= t; i++) {
+            int n = in.nextInt();
+            int ans = count(n);
+            System.out.println(ans % 2);
 
-        // }
+        }
         int[] dp = new int[5 + 1];
         System.out.println(count(5,dp));
     }
