4912809f00465919fdf46aaffde73d5d7f6e3495
prajat <rajat.singh1084@gmail.com>
2019-06-15 12:27:23 +0530

    ques solved

--- CODEZEN(Coding Ninjas)/RECURSION/createsequence.java
+++ CODEZEN(Coding Ninjas)/RECURSION/createsequence.java
@@ -1,19 +1,36 @@
 import java.util.*;
+
 /**
  * createsequence
  */
 public class createsequence {
 
-    public static void printSequence(int range) {
-        if (n < 2) {
-            return;
-        }
-        if (n / 10 == 0) {
-            System.out.println(n);
-            return;
-        }
-        printSequence(n-1);
+    public static void main(String[] args) {
+        printSequence(30, 1);
+    }
 
-        
+    public static void printSequence(int n, int start) {
+        if (start > n) {
+            return;
+        }
+
+        int temp = start;
+        boolean check = true;
+        while (temp > 0) {
+            int rem = temp % 10;
+            if (!(rem == 2 || rem == 5)) {
+
+                check = false;
+                break;
+            }
+            temp = temp / 10;
+
+        }
+        if (check == true) {
+            System.out.println(start);
+        }
+
+        printSequence(n, start + 1);
+
     }
 }
