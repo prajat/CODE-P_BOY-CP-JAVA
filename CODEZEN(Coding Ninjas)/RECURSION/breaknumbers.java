ca97eb3fa9e53d98c484b2a2084220cec6a041ef
prajat <rajat.singh1084@gmail.com>
2019-06-15 12:09:48 +0530

    changes done to ques solution

--- CODEZEN(Coding Ninjas)/RECURSION/breaknumbers.java
+++ CODEZEN(Coding Ninjas)/RECURSION/breaknumbers.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 /**
  * breaknumbers
  */
@@ -6,33 +7,10 @@ public class breaknumbers {
 
     public static void main(String[] args) {
 
-        //printCombination(4);
+        // printCombination(4);
         printPartitions(4, 4, "");
     }
-    
-    public static void printCombination(int num) {
-        ArrayList<Integer> list = new ArrayList<>();
-        printCombination(num,num,list);
-    }
 
-    public static void printCombination(int num, int remval, ArrayList<Integer> list) {
-        if (remval == 0) {
-            System.out.println(list);
-            list.clear();
-            return;
-
-        } else {
-            if (num > 1) {
-                printCombination(num - 1, remval, list);
-            }
-            if (num <= remval) {
-                list.add(num);
-                printCombination(num, remval - num, list);
-            }
-        }
-
-    }
-       
     public static void printPartitions(int target, int maxValue, String suffix) {
         if (target == 0)
             System.out.println(suffix);
