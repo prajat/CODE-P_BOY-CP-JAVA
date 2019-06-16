032dde5cb9a3392c2d10fcc478d562bbaf1a0367
prajat <rajat.singh1084@gmail.com>
2019-06-16 13:13:04 +0530

    ques solved

---
+++ CODEZEN(Coding Ninjas)/RECURSION/connectedones.java
@@ -0,0 +1,25 @@
+import java.util.*;
+/**
+ * connectedone's
+ */
+public class connectedones {
+
+    public static void main(String[] args) {
+        
+    }
+    public static int getMaxOnes(int[][] inputMatrix){
+        
+        
+        
+		return getMaxOnes(inputMatrix,0,0);
+	}
+	public static int getMaxOnes(int[][] inputMatrix, int i, int j) {
+		if(i<0 || j<0||i>=inputMatrix.length||j>=inputMatrix[0].length ||inputMatrix[i][j]==0){
+            return 0;
+        }
+
+        
+
+
+	}
+}
