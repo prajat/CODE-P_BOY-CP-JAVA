ec704296fd2a764a085e3c1b1002ee5248ad95d2
prajat <rajat.singh1084@gmail.com>
2019-06-16 13:14:09 +0530

    recursion completed

--- CODEZEN(Coding Ninjas)/RECURSION/connectedones.java
+++ CODEZEN(Coding Ninjas)/RECURSION/connectedones.java
@@ -1,25 +1,36 @@
-import java.util.*;
-/**
- * connectedone's
- */
-public class connectedones {
+public class solution {
 
-    public static void main(String[] args) {
-        
-    }
-    public static int getMaxOnes(int[][] inputMatrix){
-        
-        
-        
-		return getMaxOnes(inputMatrix,0,0);
-	}
-	public static int getMaxOnes(int[][] inputMatrix, int i, int j) {
-		if(i<0 || j<0||i>=inputMatrix.length||j>=inputMatrix[0].length ||inputMatrix[i][j]==0){
-            return 0;
+  public static int getMaxOnes(int[][] grid) {
+    int maxRegion = 0;
+
+    for (int row = 0; row < grid.length; row++) {
+      for (int col = 0; col < grid[0].length; col++) {
+        if (grid[row][col] == 1) {
+          int size = findLargestRegion(grid, row, col);
+          maxRegion = Math.max(maxRegion, size);
         }
+      }
+    }
+    return maxRegion;
+  }
 
-        
+  public static int findLargestRegion(int[][] grid, int row, int col) {
 
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+      return 0;
+    } else if (grid[row][col] == 0) {
+      return 0;
+    }
 
-	}
+    grid[row][col] = 0;
+    int size = 1;
+
+    for (int r = row - 1; r <= row + 1; r++) {
+      for (int c = col - 1; c <= col + 1; c++) {
+        size += findLargestRegion(grid, r, c);
+      }
+    }
+
+    return size;
+  }
 }
