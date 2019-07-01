c0eea651b494cc8fd85ef0b5dcfd84d9f83f3ceb
prajat <rajat.singh1084@gmail.com>
2019-07-01 23:36:02 +0530

    ques solved

---
+++ LeetCode/LinkedList/203.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode curr = head;
+        ListNode prev = dummy;
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = prev.next;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+
+    }
+}
