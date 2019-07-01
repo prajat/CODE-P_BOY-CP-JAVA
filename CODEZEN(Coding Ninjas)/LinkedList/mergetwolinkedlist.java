90ce874c2d379b073daaf8eba7d07d90f1f20612
prajat <rajat.singh1084@gmail.com>
2019-07-01 22:47:59 +0530

    ques solved

---
+++ CODEZEN(Coding Ninjas)/LinkedList/mergetwolinkedlist.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class ListNode<t> {
+    public t data;
+    public ListNode<t> next;
+
+    public ListNode(t data) {
+        this.data = data;
+    }
+}
+
+public class solution {
+
+    public static ListNode<Integer> merge(ListNode<Integer> head1, ListNode<Integer> head2) {
+
+        
+        
+
+    }
+}
