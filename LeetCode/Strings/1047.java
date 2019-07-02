7203ede05891ee8fac46f77e6f5a26c0bc9320ed
prajat <rajat.singh1084@gmail.com>
2019-07-02 13:15:20 +0530

    code optimized

--- LeetCode/Strings/1047.java
+++ LeetCode/Strings/1047.java
@@ -6,14 +6,11 @@ class Solution {
 
         char curr = S.charAt(0);
         String smallans = removeDuplicates(S.substring(1));
-        if (smallans.length() >= 2) {
+
+        if (smallans.length() >= 1) {
             if (smallans.charAt(0) == curr) {
                 return smallans.substring(1);
             }
-
-        }
-        if (smallans.length() == 1 && smallans.charAt(0) == curr) {
-            return "";
         }
 
         return curr + smallans;
