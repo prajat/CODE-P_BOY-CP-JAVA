ff96a2c8a9e4f6906b28172bbbb9a88456db4886
prajat <rajat.singh1084@gmail.com>
2019-07-03 00:11:46 +0530

    ques solved

---
+++ LeetCode/Tree/199-rightviewoftree.java
@@ -0,0 +1,39 @@
+import java.awt.List;
+import java.util.*;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == size - 1) {
+                    list.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+        }
+        return list;
+
+    }
+
+}
