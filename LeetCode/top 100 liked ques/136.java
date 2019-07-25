54fddf82ce9f97c7989f8d660ec84a2203760e6e
prajat <rajat.singh1084@gmail.com>
2019-07-25 17:48:15 +0530

    ques solved

--- LeetCode/top 100 liked ques/136.java
+++ LeetCode/top 100 liked ques/136.java
@@ -1,13 +1,13 @@
 import java.util.*;
+
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans=0;
-        for(int i=0;i<nums.length;i++){
-            ans=ans^nums[i];
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
         }
 
         return ans;
-        
-        
+
     }
 }
