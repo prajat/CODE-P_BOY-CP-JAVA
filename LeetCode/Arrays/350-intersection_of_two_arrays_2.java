ab5f79610f96a0f7dd21cc5825c36d6fd136550a
prajat <rajat.singh1084@gmail.com>
2019-08-04 23:45:35 +0530

    ques solved

--- LeetCode/Arrays/350-intersection_of_two_arrays_2.java
+++ LeetCode/Arrays/350-intersection_of_two_arrays_2.java
@@ -5,7 +5,7 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums1.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
         }
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < nums2.length; i++) {
