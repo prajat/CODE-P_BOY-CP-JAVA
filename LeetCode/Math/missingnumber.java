bbb55c1e361a2213b1cb619c530d2823331d1f55
prajat <rajat.singh1084@gmail.com>
2019-08-05 21:38:03 +0530

    ques solved

---
+++ LeetCode/Math/missingnumber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+        for (int i : nums) {
+            sum -= i;
+        }
+        return sum;
+
+    }
+}
