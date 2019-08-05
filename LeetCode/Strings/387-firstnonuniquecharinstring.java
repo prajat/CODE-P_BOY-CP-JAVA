2262d3e0ca9fe885da9ed60e70a6a9d4c5e61c7f
prajat <rajat.singh1084@gmail.com>
2019-08-05 11:05:52 +0530

    ques solved

---
+++ LeetCode/Strings/387-firstnonuniquecharinstring.java
@@ -0,0 +1,17 @@
+import java.util.LinkedHashMap;
+
+class Solution {
+    public int firstUniqChar(String s) {
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
