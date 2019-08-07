a840f244f36f4052db516c117db5786076d6204f
prajat <rajat.singh1084@gmail.com>
2019-08-07 14:18:12 +0530

    ques solved

---
+++ LeetCode/Strings/groupAnagrams.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            char[] c = word.toCharArray();
+            Arrays.sort(c);
+            String key = new String(c);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
