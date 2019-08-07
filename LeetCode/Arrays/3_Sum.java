8a44556c28d64046e0a669e1e2f02b1f0b177b9a
prajat <rajat.singh1084@gmail.com>
2019-08-07 13:47:06 +0530

    ques solved

---
+++ LeetCode/Arrays/3_Sum.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TreeSet;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        TreeSet<String> set = new TreeSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                if (nums[i] + nums[l] + nums[r] == 0) {
+                    String str = nums[i] + ":" + nums[l] + ":" + nums[r];
+                    if (!set.contains(str)) {
+                        list.add(nums[i]);
+                        list.add(nums[l]);
+                        list.add(nums[r]);
+                        ans.add(list);
+                        list = new ArrayList<>();
+                        set.add(str);
+
+                    }
+                    l++;
+                    r--;
+
+                } else if (nums[i] + nums[l] + nums[r] < 0) {
+                    j++;
+
+                } else {
+                    k--;
+                }
+            }
+
+        }
+        return ans;
+
+    }
+}
