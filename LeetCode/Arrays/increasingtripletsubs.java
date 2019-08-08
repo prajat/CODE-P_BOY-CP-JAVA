e1306cf454a5112b7a980d27b6965f81785bb269
prajat <rajat.singh1084@gmail.com>
2019-08-08 17:46:10 +0530

    ques solved

---
+++ LeetCode/Arrays/increasingtripletsubs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= a) {
+                a = nums[i];
+            } else if (nums[i] <= b) {
+                b = nums[i];
+            } else
+                return true;
+        }
+        return false;
+
+    }
+}
