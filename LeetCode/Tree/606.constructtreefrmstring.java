fe7a198e5f91e709291c00c9df40444b7818066a
prajat <rajat.singh1084@gmail.com>
2019-08-08 18:51:24 +0530

    ques solved

---
+++ LeetCode/Tree/606.constructtreefrmstring.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public String tree2str(TreeNode t) {
+        if (t == null) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        helper(t, sb);
+        return sb.toString();
+    }
+
+    private void helper(TreeNode root, StringBuilder sb) {
+        sb.append(root.val);
+        if (root.left == null && root.right == null) {
+            return;
+        }
+        if (root.left != null) {
+            sb.append("(");
+            helper(root.left, sb);
+            sb.append(")");
+        }
+        if (root.right != null) {
+            if (root.left == null) {
+                sb.append("()");
+            }
+            sb.append("(");
+            helper(root.right, sb);
+            sb.append(")");
+        }
+    }
+}
--- LeetCode/Tree/_103-zigzagtreetraversal.java
+++ LeetCode/Tree/_103-zigzagtreetraversal.java
@@ -12,37 +12,39 @@ import javax.swing.tree.TreeNode;
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        boolean flag=true;
-        List<List<Integer>> levels =new ArrayList<>();
-        bfs(root,result,flag);       
+        boolean flag = true;
+        List<List<Integer>> levels = new ArrayList<>();
+        bfs(root, levels, flag);
+        return levels;
     }
-    public void bfs(TreeNode root,List<List<Integer>> levels ,boolean flag){
-        if(root==null){
-            return ;
+
+    public void bfs(TreeNode root, List<List<Integer>> levels, boolean flag) {
+        if (root == null) {
+            return;
         }
-        Queue<TreeNode> queue=new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            List<Integer> level=new ArrayList<>();
-            for(int i=0;i<size;i++){
-                TreeNode node=queue.poll();
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
                 level.add(node.val);
-                if(node.left!=null){
+                if (node.left != null) {
                     queue.add(node.left);
                 }
-                if(node.right!=null){
+                if (node.right != null) {
                     queue.add(node.right);
                 }
             }
-            if(flag==true){
+            if (flag == true) {
                 levels.add(level);
-                flag=false;
-            }else{
+                flag = false;
+            } else {
                 Collections.reverse(level);
                 levels.add(level);
-                flag=true;
+                flag = true;
             }
 
         }
