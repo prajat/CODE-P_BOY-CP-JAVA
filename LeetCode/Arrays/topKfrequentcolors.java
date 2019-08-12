1439166581479fa9def5af5b233b2304f6f88fe5
prajat <rajat.singh1084@gmail.com>
2019-08-12 23:26:17 +0530

    ques solved

--- LeetCode/Arrays/topKfrequentcolors.java
+++ LeetCode/Arrays/topKfrequentcolors.java
@@ -1,34 +1,20 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-
-import javafx.scene.layout.Priority;
-
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
-         
+
         for (int i : nums) {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
         ArrayList<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
         Collections.sort(list, (o1, o2) -> (o2.getValue() - o1.getValue()));
-        for (Map.Entry<Character, Integer> entry : list) {
-            for (int i = 0; i < k; i++) {
-                ans.add(entry.getValue());
+        for (Map.Entry<Integer, Integer> entry : list) {
+            ans.add(entry.getKey());
+            if (ans.size() == k) {
+                break;
             }
+
         }
         return ans;
-        
-        
-
-        
-        
-
     }
 }
