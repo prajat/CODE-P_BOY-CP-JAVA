ad72c2a413a337b380cef3bf5a06dbacb69c2638
prajat <rajat.singh1084@gmail.com>
2019-08-12 23:52:50 +0530

    solutions updated

--- LeetCode/Heaps/23-mergeksortedlist.java
+++ LeetCode/Heaps/23-mergeksortedlist.java
@@ -1,34 +1,30 @@
-import java.util.*;
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> heap=new PriorityQueue<>(new Comparator<ListNode>() {
-            public int compare(ListNode obj1,ListNode obj2){
-                if(obj1.val>obj2.val)
-                return 1;
-                else return -1;
-            }
-        });
-        for(int i=0;i<lists.length;i++){
-            ListNode currhead=lists[i];
-            while(currhead!=temp){
-                heap.offer(currhead);
-                currhead=currhead.next;
-            }
+        // PriorityQueue<ListNode> heap = new PriorityQueue((Comparator<ListNode>) (o1,
+        // o2) -> o1.val - o2.val);
+        PriorityQueue<ListNode> heap = new PriorityQueue<ListNode>((o1, o2) -> (o1.val - o2.val));
 
+        for (ListNode node : lists) {
+            if (node != null) {
+                heap.offer(node);
+            }
         }
-        ListNode ans=heap.poll();
-        for(int i=1;i<heap.size();i++){
-            ans.next=heap.poll();
+
+        ListNode pre = new ListNode(-1);
+        ListNode temp = pre;
+        while (!heap.isEmpty()) {
+            ListNode curr = heap.poll();
+            temp.next = new ListNode(curr.val);
+            if (curr.next != null) {
+                heap.offer(curr.next);
+            }
+            temp = temp.next;
         }
-        
-        return ans;
+        return pre.next;
+
     }
 }
