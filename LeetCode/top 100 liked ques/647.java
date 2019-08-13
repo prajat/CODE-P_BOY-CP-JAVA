8e26eea1ea90a5110159735644a71a7abfd55b07
prajat <rajat.singh1084@gmail.com>
2019-08-13 16:04:46 +0530

    ques solved

---
+++ LeetCode/top 100 liked ques/647.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSubstrings(String s) {
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count += expand(s, i, i);
+            count += expand(s, i, i + 1);
+
+        }
+        return count;
+
+    }
+
+    public int expand(String s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            count++;
+        }
+        return count;
+    }
+}
