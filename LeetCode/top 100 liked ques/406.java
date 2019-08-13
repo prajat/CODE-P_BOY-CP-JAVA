0f71e2b6877db30d57317a0dd68c79130f538012
prajat <rajat.singh1084@gmail.com>
2019-08-13 15:32:17 +0530

    ques solved

---
+++ LeetCode/top 100 liked ques/406.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> (b[0] == a[0] ? a[1] - b[1] : b[0] - a[0]));
+        List<int[]> res = new ArrayList<>();
+        for (int[] p : people) {
+            res.add(p[1], p);
+        }
+        return res.toArray(new int[0][0]);
+    }
+
+}
