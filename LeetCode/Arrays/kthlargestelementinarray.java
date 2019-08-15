376bead6a5d7197a0cf6b9ab694f8b8c960f42df
prajat <rajat.singh1084@gmail.com>
2019-08-16 00:18:31 +0530

    ques solved

--- LeetCode/Arrays/kthlargestelementinarray.java
+++ LeetCode/Arrays/kthlargestelementinarray.java
@@ -1,19 +1,26 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-
-import javafx.scene.layout.Priority;
-
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        // PriorityQueue<Integer> heap = new
+        // PriorityQueue<>(Collections.reverseOrder());
+        // for (int i : nums) {
+        // heap.offer(i);
+        // }
+        // int ans = 0;
+        // for (int i = 0; i < k; i++) {
+        // ans = heap.poll();
+        // }
+        // return ans;
+
+       
+       
+        //min heap method
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
         for (int i : nums) {
             heap.offer(i);
+            if (heap.size() > k) {
+                heap.poll();
+            }
         }
-        int ans = 0;
-        for (int i = 0; i < k; i++) {
-            ans = heap.poll();
-        }
-        return ans;
-
+        return heap.peek();
     }
 }
