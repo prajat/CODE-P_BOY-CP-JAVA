44229a11c473b6904f07b6d82975098c7984da83
prajat <rajat.singh1084@gmail.com>
2019-08-15 11:09:41 +0530

    ques solved

---
+++ LeetCode/Strings/438-findallanagramsinstring.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String t) {
+        ArrayList<Integer> list = new ArrayList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int counter = map.size();
+        int begin = 0;
+        int end = 0;
+        while (end < s.length()) {
+            char c = s.charAt(end);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) == 0) {
+                    counter--;
+                }
+            }
+            end++;
+
+            while (counter == 0) {
+                char tempc = s.charAt(begin);
+                if (map.containsKey(tempc)) {
+                    map.put(tempc, map.get(tempc) + 1);
+                    if (map.get(tempc) > 0) {
+                        counter++;
+                    }
+                }
+                if (end - begin == t.length()) {
+                    list.add(begin);
+                }
+                begin++;
+
+            }
+
+        }
+        return list;
+
+    }
+}
