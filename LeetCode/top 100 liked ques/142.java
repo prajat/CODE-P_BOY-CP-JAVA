88b58aacd6e69d874d8ff8f76068b6ceeea9fae1
prajat <rajat.singh1084@gmail.com>
2019-08-16 16:45:51 +0530

    ques solved

---
+++ LeetCode/top 100 liked ques/142.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list. class ListNode { int val; ListNode next;
+ * ListNode(int x) { val = x; next = null; } }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                while (head != slow) {
+                    head = head.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
