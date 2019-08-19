850ac4fd7f12c44bb8906a737030dd7864162827
prajat <rajat.singh1084@gmail.com>
2019-08-19 17:37:09 +0530

    ques solved

--- LeetCode/Math/countprime.java
+++ LeetCode/Math/countprime.java
@@ -1,14 +1,28 @@
 class Solution {
     public int countPrimes(int n) {
-        boolean[] notPrime = new boolean[n];
+        if (n == 0 || n == 1) {
+            return 0;
+        }
+        boolean[] isprime = new boolean[n];
         int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (!notPrime[i]) {
-                count++;
-                for (int j = 2; i * j < n; j++) {
-                    notPrime[i * j] = true;
+        for (int i = 0; i < n; i++) {
+            isprime[i] = true;
+        }
+        isprime[0] = false;
+
+        isprime[1] = false;
+        for (int i = 2; i * i < n; i++) {
+            if (isprime[i] == true) {
+
+                for (int j = i * i; j < n; j += i) {
+                    isprime[j] = false;
                 }
             }
+
+        }
+        for (boolean i : isprime) {
+            if (i == true)
+                count++;
         }
         return count;
     }
